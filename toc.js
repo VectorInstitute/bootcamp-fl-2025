05f83015d0402417b2df9f7a66a5cb5a28afa48b
emersodb <43939939+emersodb@users.noreply.github.com>
2025-07-14 20:48:02 +0000

    Deploying to gh-pages from @ VectorInstitute/FL4Health@b5419b6a90a2060998cfd577699573e35755d5a4 🚀

--- toc.js
+++ toc.js
@@ -10,7 +10,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
     connectedCallback() {
         this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="repo_setup_guide.html"><strong aria-hidden="true">1.</strong> Repository Setup Guide</a></li><li class="chapter-item expanded "><a href="ide_and_environment_guide.html"><strong aria-hidden="true">2.</strong> Setting Up Your IDE</a></li><li class="chapter-item expanded "><a href="repo_roadmap.html"><strong aria-hidden="true">3.</strong> Repository Roadmap</a></li><li class="chapter-item expanded "><a href="troubleshooting.html"><strong aria-hidden="true">4.</strong> Troubleshooting</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
-        let current_page = document.location.href.toString().split("#")[0];
+        let current_page = document.location.href.toString().split("#")[0].split("?")[0];
         if (current_page.endsWith("/")) {
             current_page += "index.html";
         }
